1456b35b4f4d58be212300bf3a75c4e0d8264f3c
Denis Nagibin <denis.nagibin@gmail.com>
2015-10-04 12:22:04 +0300

    Enabling CORS

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Cross-Origin Resource Sharing
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/tasks', tasks);
 
 // catch 404 and forward to error handler
